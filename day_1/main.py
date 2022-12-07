6848f73ed78e2970f7ae0a78c71ab0a1140cf55f
Kenneth Lee <kenneth.lee.developer@gmail.com>
2022-12-06 19:02:36 -0800

    NOW finished day 1 both parts

--- day_1/main.py
+++ day_1/main.py
@@ -1,12 +1,32 @@
-total = 0
-highest = 0
 
-with open('./input.txt') as file:
-  for line in file:
-    if line == "\n":
-      highest = max(highest, total)
-      total = 0
-    else:
-      total += int(line)
+def day_1_part_1():
+  total = 0
+  highest = 0
 
-print(highest)
+  with open('./input.txt') as file:
+    for line in file:
+      if line == "\n":
+        highest = max(highest, total)
+        total = 0
+      else:
+        total += int(line)
+
+  return highest
+
+print(day_1_part_1())
+
+def day_2_attempt():
+  total = 0
+  sums = []
+
+  with open('./input.txt') as file:
+    for line in file:
+      if line == '\n':
+        sums.append(total)
+        total = 0
+      else:
+        total += int(line)
+  
+  return sum(sorted(sums, reverse=True)[:3])
+    
+print(day_2_attempt())

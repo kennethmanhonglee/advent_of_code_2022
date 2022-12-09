8a31b470eec7086cd1d900ce30534cd69ccd561e
Kenneth Lee <kenneth.lee.developer@gmail.com>
2022-12-08 16:20:38 -0800

    Working on part 1, dont understand the question

--- day_6/main.py
+++ day_6/main.py
@@ -1,9 +1,23 @@
 class Day6Attempt():
   def __init__(self) -> None:
-    pass
+    self.part_1()
 
   def part_1(self):
-    pass
+    with open('./input.txt') as file:
+      code = file.read()
+      cache = dict()
+      count = 0
+
+      for i, char in enumerate(code):
+        if char not in cache:
+          if count == 4:
+            print(i)
+            return
+
+          cache[char] = count
+          count += 1
+      raise Exception("not found")
+
 
   def part_2(self):
     pass
@@ -11,4 +25,3 @@ class Day6Attempt():
 
 if __name__ == "__main__":
   Day6Attempt()
-  

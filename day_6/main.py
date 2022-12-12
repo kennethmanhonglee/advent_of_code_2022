b8e25b4d5a2cd6add2c401f6a5aa7662fb224b5f
Kenneth Lee <kenneth.lee.developer@gmail.com>
2022-12-11 19:40:02 -0800

    Finished day 6 both parts!!

--- day_6/main.py
+++ day_6/main.py
@@ -1,21 +1,31 @@
 class Day6Attempt():
   def __init__(self) -> None:
-    self.part_1()
+    self.part_1(distinct_num_count=4)  # part 1
+    self.part_1(distinct_num_count=14)  # part 2
 
-  def part_1(self):
+  def part_1(self, distinct_num_count: int):
     with open('./input.txt') as file:
+      """
+      use a sliding window
+
+      4 chars
+      if there are repeated chars within the 4 chars in the window:
+          move the slower index / pointer + 1
+      else return faster index
+      """
+
+      i, j = 0, distinct_num_count
       code = file.read()
-      cache = dict()
-      count = 0
+      
+      while j < len(code) - 1:
+        if len(set(code[i:j])) == len(code[i:j]) == distinct_num_count:
+          print(j)
+          return j
+        else:
+          i += 1
+          j += 1
 
-      for i, char in enumerate(code):
-        if char not in cache:
-          if count == 4:
-            print(i)
-            return
 
-          cache[char] = count
-          count += 1
       raise Exception("not found")
 
 
